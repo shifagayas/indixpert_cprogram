4838c817c9a3953f009a9ceb65c26ed0ec590162
Priyanshu7255 <priyanshu23242526@gmail.com>
2024-07-04 15:07:30 +0530

    numberfromuser

---
+++ priyanshu7255/number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+
+  int number[10];
+  
+  printf("please enter any number:");
+  scanf("%d",&number);
+  printf("\n%d", number);
+
+
+
+
+
+
+
+
+
+    return 0;
+}
