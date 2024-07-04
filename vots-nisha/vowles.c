5ab34c3331c6dacf335b4d1158496310df263630
nisha515 <nishamehtaa@example.com>
2024-07-04 15:55:05 +0530

    vowles.c

---
+++ vots-nisha/vowles.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+
+printf("this this even number: ");
+ 
+ for(int i=0; i<=20; i++)
+ {
+
+    if(i % 2==0){
+
+        printf("\n%d" ,i);
+    }
+ }
+
+
+
+
+
+ 
+
+
+    return 0;
+}
