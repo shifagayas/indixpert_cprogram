d702071dd07218d2668771495c637bc95afdaeed
Kumkum555 <Kumkumkapkoti@gmail.com>
2024-07-04 15:51:17 +0530

    alphabet

---
+++ vots-kumkum/alphabet.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+
+char ch;
+
+for(ch ='a' ;ch <= 'z'; ch++){
+    printf("%c",ch);
+}
+
+printf("\n");
+
+
+for(ch ='a';ch <='z'; ch++){
+    printf("%c ",ch);
+}
+
+printf("\n");
+
+return 0;
+
+}
